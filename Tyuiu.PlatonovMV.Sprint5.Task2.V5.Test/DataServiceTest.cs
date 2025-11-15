0f973ec5eae02fb9fb32bbe317d21cebbb6fd1ce
Максим Платонов <Tyuiu.PlatonovMV@outlook.com>
2025-11-15 21:30:04 +0500

    Task2

---
+++ Tyuiu.PlatonovMV.Sprint5.Task2.V5.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Tyuiu.PlatonovMV.Sprint5.Task2.V5.Lib;
+
+namespace Tyuiu.PlatonovMV.Sprint5.Task2.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckSaveToFileTextData()
+        {
+            int[,] matrix = new int[3, 3]
+            {
+                { 9, 6, 6 },
+                { 8, 7, 2 },
+                { 1, 7, 8 }
+            };
+
+            DataService ds = new DataService();
+            string path = ds.SaveToFileTextData(matrix);
+
+            bool fileExists = File.Exists(path);
+            Assert.AreEqual(true, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.PlatonovMV.Sprint5.Task2.V5/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Tyuiu.PlatonovMV.Sprint5.Task2.V5.Lib;
+
+namespace Tyuiu.PlatonovMV.Sprint5.Task2.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Платонов М. В. | ИСТНб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Платонов Максим Владимирович | ИСТНб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры. Заменить нечетные элементы массива на 0.       *");
+            Console.WriteLine("* Результат сохранить в файл OutPutFileTask2.csv и вывести на консоль.    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] matrix = new int[3, 3];
+
+            Console.WriteLine("Введите 9 элементов массива 3x3:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"Элемент [{i},{j}]: ");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nИсходный массив:");
+            PrintMatrix(matrix);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            string path = ds.SaveToFileTextData(matrix);
+
+            Console.WriteLine("Преобразованный массив (нечетные элементы заменены на 0):");
+            PrintMatrix(GetTransformedMatrix(matrix));
+
+            Console.WriteLine("\nРезультат сохранен в файл: " + path);
+
+            
+            Console.WriteLine("\nСодержимое файла:");
+            string[] csvLines = File.ReadAllLines(path);
+            foreach (string line in csvLines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j],4}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static int[,] GetTransformedMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            int[,] result = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    int value = matrix[i, j];
+                    if (value % 2 != 0)
+                    {
+                        result[i, j] = 0;
+                    }
+                    else
+                    {
+                        result[i, j] = value;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
